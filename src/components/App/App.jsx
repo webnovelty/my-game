490a723814d56bec65c223b6098dc36df47b3f44
webnovelty <webnoveltystudio@gmail.com>
2022-10-23 23:05:04 +0200

    v1.2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,54 +1,110 @@
-import { Game, Me, MeMe, PtnHl, Title } from './App.styled';
+import safe from 'img/safeme.png';
+import gameover from 'img/gameover.png';
 import { useEffect, useState } from 'react';
+import { Footer, Game, Me, PtnHl, Title } from './App.styled';
 
 const App = () => {
-	const [jump, setJump] = useState(false);
-	const [enter, setEnter] = useState(false);
-	const [welcome, setWelcome] = useState(true);
+  const meSelf = document.getElementsByClassName('meHero');
+  const putinHuilo = document.getElementsByClassName('huilo');
 
-	useEffect(() => {
+  const [enter, setEnter] = useState(false);
+  const [welcome, setWelcome] = useState(true);
+  const [gameOver, setGameOver] = useState(false);
+
+  const jump = () => {
+    if (meSelf[0].classList !== 'jump') {
+      meSelf[0].classList.add('jump');
+    }
+    setTimeout(() => {
+      meSelf[0].classList.remove('jump');
+    }, 600);
+  };
+
+  useEffect(() => {
     const keyDown = e => {
       if (e.code === 'Enter') {
-		  setEnter(true);
-		  setWelcome(false);
+        setEnter(true);
+        setWelcome(false);
       }
     };
     window.addEventListener('keydown', keyDown);
     return () => {
       window.removeEventListener('keydown', keyDown);
     };
-  },[]);
+  }, []);
 
-	useEffect(() => {
-		
-		const keyDown = e => {
-			if (e.code === 'Space') {
-				setJump(true);
-			}
-		}
-		window.addEventListener('keydown', keyDown);
-		setTimeout(() => { setJump(false) }, 700);
-		return () => {
-			window.removeEventListener('keydown', keyDown)
-		};	
-	});
+  useEffect(() => {
+    const keyDown = e => {
+      if (e.code === 'Space') {
+        jump();
+      }
+    };
+    window.addEventListener('keydown', keyDown);
+    return () => {
+      window.removeEventListener('keydown', keyDown);
+    };
+  });
 
+  useEffect(() => {
+    setInterval(function () {
+      const meSelfTop = parseInt(
+        window.getComputedStyle(meSelf[0]).getPropertyValue('top')
+      );
+      const putinHuiloTop = parseInt(
+        window.getComputedStyle(putinHuilo[0]).getPropertyValue('left')
+      );
+      console.log(putinHuiloTop);
+      if (putinHuiloTop === 70 && putinHuiloTop > 0 && meSelfTop >= 140) {
+        setGameOver(true);
+      }
+    }, 10);
+  });
 
-	return (
-    <main>
-      {welcome && <Title>Чтобы начать, тыкни на Enter :)</Title>}
+  useEffect(() => {
+    if (!gameOver) {
+      return;
+    }
+    const keyDown = e => {
+      if (e.code === 'Enter') {
+        setGameOver(false);
+        setEnter(true);
+        setWelcome(false);
+      }
+    };
+    window.addEventListener('keydown', keyDown);
+    return () => {
+      window.removeEventListener('keydown', keyDown);
+    };
+  }, [gameOver]);
 
-      {enter && (
-        <>
-          <Title>Тыкай пробел чтобы прыгнуть</Title>
-          <Game>
-            {jump && <MeMe></MeMe>}
-            {!jump && <Me></Me>}
-            <PtnHl></PtnHl>
-          </Game>
-        </>
-      )}
-    </main>
+  return (
+    <>
+      <main>
+        {gameOver && (
+          <>
+            <Title>НЕ СПИ!, хочешь еще? ЖМИ Enter! :)</Title>
+            <img src={gameover} alt="safe" />
+          </>
+        )}
+        {welcome && !gameOver && (
+          <>
+            <Title>Чтобы начать, тыкни на Enter :)</Title>
+            <img src={safe} alt="safe" />
+          </>
+        )}
+
+        {enter && !gameOver && (
+          <>
+            <Title>Тыкай пробел чтобы прыгнуть</Title>
+            <Game>
+              <Me className="meHero"></Me>
+              <PtnHl className="huilo"></PtnHl>
+            </Game>
+          </>
+        )}
+      </main>
+      <Footer>v1.2 Спасти рядового Анатолия &#169; 2022</Footer>
+    </>
   );
 };
 
--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,17 +1,4 @@
-import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
-import me from 'img/me.png';
-import ptnhl from 'img/putin-huilo.png';
-
-export const huiloMove = keyframes`
- 0% { left: 700px }
- 100% { left: -80px }
-`;
-export const jumpHero = keyframes`
- 0% { top: 150px }
- 50% {top: 0px}
- 100% { top: 150px }
-`;
 
 export const Title = styled.div`
 text-align: center;
@@ -21,6 +8,7 @@ width: 800px;
 margin: 50px auto 0px auto;
 `;
 
+
 export const Game = styled.div`
 width: 800px;
 height: 350px;
@@ -29,44 +17,14 @@ margin: auto;
 `;
 
 export const Me = styled.div`
-width: 450px;
-height: 350px;
-background-image: url(${me});
-background-size: 450px 250px;
-background-repeat: no-repeat;
-position: relative;
-top: 150px;
-left: -100px;
 `;
 
 export const PtnHl = styled.div`
-width: 80px;
-height: 80px;
-background-image: url(${ptnhl});
-background-size: 80px 80px;
-background-repeat: no-repeat;
-position: relative;
-top: -80px;
-left: 700px;
- animation-name: ${huiloMove};
- animation-duration: 2s;
- animation-iteration-count: infinite;
 `;
 
-export const Jump = styled.div`
- animation-name: ${jumpHero};
- animation-duration: 1s;
-`;
-
-export const MeMe = styled.div`
-width: 450px;
-height: 350px;
-background-image: url(${me});
-background-size: 450px 250px;
-background-repeat: no-repeat;
-position: relative;
-top: 100px;
-left: -100px;
- animation-name: ${jumpHero};
- animation-duration: 1s;
+export const Footer = styled.div`
+padding-top: 100px;
+display: block;
+text-align: center;
+margin: auto;
 `;

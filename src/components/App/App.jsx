c0220cf2bc9cd7929d9ff29024bfe978618c6212
webnovelty <webnoveltystudio@gmail.com>
2022-10-23 19:37:10 +0200

    v1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,54 @@
-import { Game, Me, PtnHl } from './App.styled';
+import { Game, Me, MeMe, PtnHl, Title } from './App.styled';
+import { useEffect, useState } from 'react';
 
 const App = () => {
-  return (
-    <Game>
-		  <Me></Me>
-		  <PtnHl></PtnHl>
-    </Game>
+	const [jump, setJump] = useState(false);
+	const [enter, setEnter] = useState(false);
+	const [welcome, setWelcome] = useState(true);
+
+	useEffect(() => {
+    const keyDown = e => {
+      if (e.code === 'Enter') {
+		  setEnter(true);
+		  setWelcome(false);
+      }
+    };
+    window.addEventListener('keydown', keyDown);
+    return () => {
+      window.removeEventListener('keydown', keyDown);
+    };
+  },[]);
+
+	useEffect(() => {
+		
+		const keyDown = e => {
+			if (e.code === 'Space') {
+				setJump(true);
+			}
+		}
+		window.addEventListener('keydown', keyDown);
+		setTimeout(() => { setJump(false) }, 700);
+		return () => {
+			window.removeEventListener('keydown', keyDown)
+		};	
+	});
+
+
+	return (
+    <main>
+      {welcome && <Title>Чтобы начать, тыкни на Enter :)</Title>}
+
+      {enter && (
+        <>
+          <Title>Тыкай пробел чтобы прыгнуть</Title>
+          <Game>
+            {jump && <MeMe></MeMe>}
+            {!jump && <Me></Me>}
+            <PtnHl></PtnHl>
+          </Game>
+        </>
+      )}
+    </main>
   );
 };
 
--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,9 +1,72 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
+import me from 'img/me.png';
+import ptnhl from 'img/putin-huilo.png';
+
+export const huiloMove = keyframes`
+ 0% { left: 700px }
+ 100% { left: -80px }
+`;
+export const jumpHero = keyframes`
+ 0% { top: 150px }
+ 50% {top: 0px}
+ 100% { top: 150px }
+`;
+
+export const Title = styled.div`
+text-align: center;
+font-weight: 900;
+font-size: 50px;
+width: 800px;
+margin: 50px auto 0px auto;
+`;
 
 export const Game = styled.div`
-width: 600px;
-height: 200px;
-border-bottom: 1px solid black;
+width: 800px;
+height: 350px;
+border-bottom: 2px solid black;
+margin: auto;
 `;
-export const Me = styled.div``;
-export const PtnHl = styled.div``;
+
+export const Me = styled.div`
+width: 450px;
+height: 350px;
+background-image: url(${me});
+background-size: 450px 250px;
+background-repeat: no-repeat;
+position: relative;
+top: 150px;
+left: -100px;
+`;
+
+export const PtnHl = styled.div`
+width: 80px;
+height: 80px;
+background-image: url(${ptnhl});
+background-size: 80px 80px;
+background-repeat: no-repeat;
+position: relative;
+top: -80px;
+left: 700px;
+ animation-name: ${huiloMove};
+ animation-duration: 2s;
+ animation-iteration-count: infinite;
+`;
+
+export const Jump = styled.div`
+ animation-name: ${jumpHero};
+ animation-duration: 1s;
+`;
+
+export const MeMe = styled.div`
+width: 450px;
+height: 350px;
+background-image: url(${me});
+background-size: 450px 250px;
+background-repeat: no-repeat;
+position: relative;
+top: 100px;
+left: -100px;
+ animation-name: ${jumpHero};
+ animation-duration: 1s;
+`;

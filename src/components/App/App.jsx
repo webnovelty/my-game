eea1fc5305688a93801523719c35357af2544fca
webnovelty <webnoveltystudio@gmail.com>
2022-10-23 15:16:50 +0200

    begin

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { Game, Me, PtnHl } from './App.styled';
+
+const App = () => {
+  return (
+    <Game>
+		  <Me></Me>
+		  <PtnHl></PtnHl>
+    </Game>
+  );
+};
+
+export default App;
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Game = styled.div`
+width: 600px;
+height: 200px;
+border-bottom: 1px solid black;
+`;
+export const Me = styled.div``;
+export const PtnHl = styled.div``;
